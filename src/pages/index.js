a8e0be253fbde420266a280cc3a9922157dcbf94
Haru-Tachibana <paloma030415@gmail.com>
2025-08-01 14:31:02 +0800

    .

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import * as React from 'react'
 import Layout from '../components/layout'
 import { StaticImage } from 'gatsby-plugin-image'
 import { FaGithub, FaLinkedin, FaInstagram } from 'react-icons/fa'
-import { HeroSection, BigHeading, AccentText, SubHeading } from '../components/styled';
+import { HeroSection, BigHeading, AccentText, SubHeading, BodyText } from '../components/styled';
 
 const IndexPage = () => {
   return (
@@ -12,6 +12,7 @@ const IndexPage = () => {
         <BigHeading>Hi. I'm Yuyang Wang.</BigHeading>
         <SubHeading>Currently doing MSc Software Engineering at NTU.</SubHeading>
         <SubHeading>A data science enthusiast, bassist, anime fan, and dog lover...here is my pup Kemi, she's a happy girl and loves to play :3</SubHeading>
+        <BodyText>This is my personal website. Feel free to explore my experiences and interests..(maybe some fun facts as well)</BodyText>
         <figure>
           <StaticImage src="../images/kemi1.jpg" />
         </figure>
